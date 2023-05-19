c43e70930915417706c62a3eb3e3a34d032bb0aa
Aarushi Sharma <107745284+aarushiiii02@users.noreply.github.com>
2023-05-19 15:05:19 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket = "backend-aarushi"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
-    dynamodb_table = "aarushi-TB"
-  }
+    backend "s3" {}
 }
